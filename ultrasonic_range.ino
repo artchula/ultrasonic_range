9ecf8aa39a8f3907c4ef15b598efaa4acac7cc78
Arkom Thaicharoen <artchula@gmail.com>
2023-06-13 19:03:09 +0700

    Initial commit

---
+++ ultrasonic_range.ino
@@ -0,0 +1,33 @@
+#include <Maqueen.h>
+Maqueen bot;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+Adafruit_Microbit microbit;
+
+void setup() {
+  Serial.begin(9600);
+  bot.begin();
+}
+
+void loop() {
+  unsigned int rangeping = sonar.ping();
+  unsigned long rangeinch = sonar.ping_in();
+  unsigned long rangecm = sonar.ping_cm();
+  unsigned long rangeping_median = sonar.ping_median(); //ค่าของข้อมูลที่อยู่ตรงกลางจากข้อมูลทั้งหมด
+
+  Serial.print("Distance: ");
+  Serial.print(rangeping);
+  Serial.print(" ping \t");
+
+  Serial.print("Distance: ");
+  Serial.print(rangeping_median);
+  Serial.print(" median \t");
+
+  Serial.print("Distance: ");
+  Serial.print(rangeinch);
+  Serial.print(" inch \t");
+
+  Serial.print("Distance: ");
+  Serial.print(rangecm);
+  Serial.println(" cm");
+  delay(10);
+}
